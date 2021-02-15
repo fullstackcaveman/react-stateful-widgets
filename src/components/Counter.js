dc6f51c38bb090733405310e76f70d42ca7cc36c
Chris Burton <chris@fullstackcaveman.com>
2021-02-14 23:02:38 -0700

    Refactored Counter.js increment and decrement functions to use prevCount

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -52,10 +52,10 @@ export default function Counter() {
 	const [count, setCount] = useState(0);
 
 	const increment = () => {
-		setCount(count + 1);
+		setCount((prevCount) => prevCount + 1);
 	};
 	const decrement = () => {
-		setCount(count - 1);
+		setCount((prevCount) => prevCount - 1);
 	};
 	const reset = () => {
 		setCount(0);
