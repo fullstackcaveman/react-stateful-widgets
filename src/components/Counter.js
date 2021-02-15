35361a9ee2447694e1a9b8e43656178ad6d2c8de
Chris Burton <chris@fullstackcaveman.com>
2021-02-14 22:07:00 -0700

    Solved Moods.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -49,7 +49,6 @@ STEP 6:
 import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-	/* STEP 1 */
 	const [count, setCount] = useState(0);
 
 	const increment = () => {
